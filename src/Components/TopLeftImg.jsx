d56d5c6277c2cc550d06f9198d4323dd97e1ff98
Cruz Maldonado <cruzmaldonadocharris@gmail.com>
2023-09-17 18:34:57 -0400

    se pasan todas las imagenes, componentes iniciales y la configuracion de tailwind.config

---
+++ src/Components/TopLeftImg.jsx
@@ -0,0 +1,11 @@
+const TopLeftImg = () => {
+  return (
+  
+  <div className="absolute left-0 top-0 mix-blend-color-dodge z-10 w-200 h-200 lg:w-400 lg:h-400 opacity-50">
+      <div className="w-full h-full bg-cover bg-center bg-TopLeftImg"></div>
+    </div>
+    
+    )
+};
+
+export default TopLeftImg;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,55 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    container: {
+      padding: {
+        DEFAULT: '15px',
+      },
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+      xl: '1200px',
+    },
+    extend: {
+      colors: {
+        primary: '#131424',
+        secondary: '#393A47',
+        accent: '#F13024',
+      },
+      backgroundImage: {
+        explosion: 'url("./src/images/bg-explosion.png")',
+        circles: 'url("./src/images/bg-circles.png")',
+        circleStar: 'url("./src/images/circle-star.svg")',
+        site: 'url("./src/images/site-bg.svg")',
+        TopLeftImg: 'url("./src/images/top-left-img.png")',
+        
+      },
+      height:{
+        '400':'25rem',
+        '200':'12.5rem'
 
+      },
+      width:{
+        '400':'25rem',
+        '200':'12.5rem',
+      },
+      animation: {
+        'spin-slow': 'spin 6s linear infinite',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        sora: ['Sora', 'sans-serif'],
+      },
+      
+      
+    },
+  },
+  container: {
+    padding: {
+      DEFAULT: '15px',
+    },
+  },
+  plugins: [require('tailwind-scrollbar')],
+}
