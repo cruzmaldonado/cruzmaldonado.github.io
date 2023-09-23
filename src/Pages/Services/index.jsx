faa182f4e17fe1f3b8d20774588f108a8e5f73f5
Cruz Maldonado <cruzmaldonadocharris@gmail.com>
2023-09-22 20:49:52 -0400

    se termina la seccion de my services, falta cambiar los textos, ademas se detecto que en la seccion about en la linea 204 el .map no se esta asignando un key unica a cada elemento, ademas de que swiger posiblemente deje de funcionar ,esta dependencia tendra que hacer una migracion

--- src/Pages/Services/index.jsx
+++ src/Pages/Services/index.jsx
@@ -1,54 +1,68 @@
-
-import { AnimatePresence,motion } from "framer-motion";
-import Layout from "../../Components/Layout"
+import { AnimatePresence, motion } from "framer-motion";
+import Layout from "../../Components/Layout";
 import Transition from "../../Components/Transition";
 
-import {} from "react-icons/rx"
-
-export const serviceData =[]
-
-import ServiceSlider from "../../Components/ServiceSlider"
-import Buld from "../../Components/Bulb"
-import Circles from "../../Components/Circles"
-import {fadeIn} from "../../../variants"
+import ServiceSlider from "../../Components/ServiceSlider";
+import Buld from "../../Components/Bulb";
+import Circles from "../../Components/Circles";
+import { fadeIn } from "../../../variants";
 
 const Services = () => {
-  return(
+  return (
+    <Layout>
+      <AnimatePresence mode="wait">
+        <motion.div className="h-full">
+          <Transition />
 
-<Layout>
-      < AnimatePresence mode ="wait">
+          <div className="h-full bg-primary/30 py-36 items-center">
+            <Circles />
+            <div className="container mx-auto ">
+              <div className="flex flex-col xl:flex-row gap-x-8">
+                {/*  text*/}
+                <div
+                  className="text-center flex xl:w-[30vw] flex-col lg:text-left
+      mb-4 xl:mb-0"
+                >
+                  <motion.h2
+                    variants={fadeIn("up", 0.2)}
+                    initial="hidden"
+                    animate="show"
+                    exit="hidden"
+                    className="h2 xl:mt-8"
+                  >
+                    My services <span className="text-accent">.</span>
+                  </motion.h2>
+                  <motion.p 
+                  variants={fadeIn("up ", 0.4)}
+                  initial="hidden"
+                  animate="show"
+                  exit="hidden"
+                  className="mb-4 max-w-[400px] mx-auto lg:mx-0">
+                    Lorem, ipsum d adipisicing elit. Quis voluptatum, motur
+                    illo?,Lorem, ipsum d adipisicing elit. Quis voluptatum,
+                    motur illo?,Lorem, ipsum d adipisicing elit. Quis
+                    voluptatum, motur illo?
+                  </motion.p>
+                </div>
 
-    <motion.div className='h-full'>
 
-<Transition/>
-
-
-
-<div className="h-full bg-primary/30 py-36 items-center">
-  <Circles/>
-  <div className="container mx-auto ">
-    <div className="flex flex-col xl:flex-row gap-x-8">
-      {/*  text*/}
-      <div className="text-center flex xl:w-[30vw] flex-col lg:text-left
-      mb-4 xl:mb-0">
-        <h2 className="h2 xl:mt-8">My services <span className="text-accent">.</span></h2>
-          <p className="mb-4 max-w-[400px] mx-auto lg:mx-0">Lorem, ipsum d adipisicing elit. Quis voluptatum, motur illo?,Lorem, ipsum d adipisicing elit. Quis voluptatum, motur illo?,Lorem, ipsum d adipisicing elit. Quis voluptatum, motur illo?
-
-          </p>
-      </div>
-      {/* slider */}
-      <ServiceSlider/>
-    </div>
-  </div>
-  <Buld/>
-</div>
-
-
-
-    </motion.div>
+                <motion.div 
+                variants={fadeIn("down", 0.6)}
+                initial="hidden"
+                animate="show"
+                exit="hidden"
+                className="w-full xl:max-w-[65%]">
+                  {/* slider */}
+                  <ServiceSlider />
+                </motion.div>
+              </div>
+            </div>
+            <Buld />
+          </div>
+        </motion.div>
       </AnimatePresence>
-</Layout>
-  )
+    </Layout>
+  );
 };
 
 export default Services;
